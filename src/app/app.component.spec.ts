990375cd7dc71a8924945878c5965978dbed4a32
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-05-26 19:24:30 +0530

    name updated to innov8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'innov-8'`, () => {
+  it(`should have as title 'innov8'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('innov-8');
+    expect(app.title).toEqual('innov8');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('innov-8 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('innov8 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'innov-8';
+  title = 'innov8';
 }
