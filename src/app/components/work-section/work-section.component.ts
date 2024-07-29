3b58fb1396788fd2df6942ecac7f04611133d1ba
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-07-29 10:57:16 +0530

    dynamic carousal update

--- src/app/components/work-section/work-section.component.ts
+++ src/app/components/work-section/work-section.component.ts
@@ -10,6 +10,7 @@ import { environment } from 'src/environments/environment';
 })
 export class WorkSectionComponent {
   data: any = []
+  works: any = []
   attributesMapping = {
     work: 'work',
     title: 'title',
@@ -19,12 +20,18 @@ export class WorkSectionComponent {
   }
   dots = new Array(5);
   activeIndex: number | null = null;
-
+  intervalId: any;
   constructor(private _dataService: DataService, private googleSheetsDbService: GoogleSheetsDbService) { }
   ngOnInit(): void {
     this.fetchData();
+    this.onDotClick(0)
+    this.startAutoCycle();
   }
-
+  startAutoCycle() {
+    this.intervalId = setInterval(() => {
+      this.onDotClick((this.currentIndex + 1) % this.data.length);
+    }, 3000);
+  }
   fetchData(): void {
     // this._dataService.getWorkData('Sheet1!A1: D10').subscribe((response: any) => {
     //   this.data = response.values;
@@ -35,11 +42,34 @@ export class WorkSectionComponent {
       "Sheet1",
       this.attributesMapping
     ).subscribe(res => {
+      this.works = res
       this.data = res
+      this.onDotClick(0)
     })
   }
 
-  onDotClick(index: number): void {
-    this.activeIndex = index;
+
+  currentIndex = 0;
+
+  onDotClick(index: number) {
+    this.currentIndex = index;
+  }
+
+  getBoxClass(index: number): string {
+    const position = (index - this.currentIndex + this.data.length) % this.data.length;
+    switch (position) {
+      case 0:
+        return 'box-3';
+      case 1:
+        return 'box-4';
+      case 2:
+        return 'box-5';
+      case 3:
+        return 'box-1';
+      case 4:
+        return 'box-2';
+      default:
+        return '';
+    }
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    googleApiKey: 'AIzaSyCOB8mscqDNgZtXAcyUaZts8PFPdbw6Ric',
+    spreadsheetId: '15JF1YhAu0sTSkMZRult5L-gYupiuL6IJaAaDjfZDsnI',
+};
